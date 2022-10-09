f48e86593f6932b1d886b838f2f1767baedc752d
Pascal Noisette <netpascal0123@aol.com>
2022-10-09 10:59:56 +0200

    create wishlist before

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { uid } from 'uid/secure';
 import Link from '../src/Link';
 
 export default function Index() {
-  const [id, setId] = React.useState("");
 
-  React.useEffect(() => {
-    setId(uid(32));
-  }, []);
   return (
     <Box minHeight="80vh" display="flex" justifyContent="center" alignItems="center">
-      {id.length && (
-        <Link href={`list/${id}`} color="secondary">
-          + Create a new wishlist
-        </Link>
-      )}
+      <Link href={`api/create`} color="secondary">
+        + Create a new wishlist
+      </Link>
     </Box>
   );
 }
